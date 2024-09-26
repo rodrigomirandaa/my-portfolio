952dda45f396d3128d429af3ee341e9aca007a85
rodrigomirandaa <digoracm@gmail.com>
2024-09-25 21:59:17 -0300

    fix: project title

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,14 +27,14 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Make Your Day",
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    title: "Study Hub",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
